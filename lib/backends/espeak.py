007f493f581a9f5afb48bad3288cd45d48e84ea5
Peter Vágner <pvdeejay@gmail.com>
2014-03-16 21:03:17 +0100

    ESpeakTTSBackend: Added voices selection.

--- lib/backends/espeak.py
+++ lib/backends/espeak.py
@@ -2,6 +2,20 @@
 from base import TTSBackendBase
 import ctypes
 import ctypes.util
+import os
+
+class espeak_VOICE(ctypes.Structure):
+	_fields_=[
+		('name',ctypes.c_char_p),
+		('languages',ctypes.c_char_p),
+		('identifier',ctypes.c_char_p),
+		('gender',ctypes.c_byte),
+		('age',ctypes.c_byte),
+		('variant',ctypes.c_byte),
+		('xx1',ctypes.c_byte),
+		('score',ctypes.c_int),
+		('spare',ctypes.c_void_p),
+	]
 
 class ESpeakTTSBackend(TTSBackendBase):
 	provider = 'eSpeak'
@@ -12,11 +26,13 @@ class ESpeakTTSBackend(TTSBackendBase):
 		self.eSpeak.espeak_Initialize(0,0,None,0)
 		
 	def say(self,text,interrupt=False):
+		voice = self.currentVoice()
+		if voice: self.eSpeak.espeak_SetVoiceByName(voice)
 		if interrupt: self.eSpeak.espeak_Cancel()
 		if isinstance(text,unicode): text = text.encode('utf-8')
 		sb_text = ctypes.create_string_buffer(text)
 		size = ctypes.sizeof(sb_text)
-		self.eSpeak.espeak_Synth(text,size,0,0,0,0x1000,None,None)
+		self.eSpeak.espeak_Synth(sb_text,size,0,0,0,0x1000,None,None)
 	
 	def close(self):
 		self.eSpeak.espeak_Cancel()
@@ -25,3 +41,15 @@ class ESpeakTTSBackend(TTSBackendBase):
 	@staticmethod
 	def available():
 		return bool(ctypes.util.find_library('espeak'))
+
+	def voices(self):
+		voices=self.eSpeak.espeak_ListVoices(None)
+		aespeak_VOICE=ctypes.POINTER(ctypes.POINTER(espeak_VOICE))
+		pvoices=ctypes.cast(voices,aespeak_VOICE)
+		voiceList=[]
+		index=0
+		while pvoices[index]:
+			voiceList.append(os.path.basename(pvoices[index].contents.identifier))
+			index+=1
+		return voiceList
+
